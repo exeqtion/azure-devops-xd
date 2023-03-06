82b9fe2c29ca9488660fe1f0db312feecd00e9d5
exeqtion <67519576+exeqtion@users.noreply.github.com>
2023-03-06 16:59:52 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "AYOOOOO";
+        return "AYOOOOO !!!!!!!";
     }
 
     public static void main(String[] args) {
