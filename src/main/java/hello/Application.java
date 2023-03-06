61937c2a4bd58b1c623f94f807682df1489c0a33
Purvesh Munagekar <pmunagekar@AD.NSEIT.COM>
2023-03-06 15:22:38 +0530

    xd

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Tuturu~";
+        return "AYOOOOO";
     }
 
     public static void main(String[] args) {
