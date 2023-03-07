85143a0477eb4a2f98dd00d509e575a09ceab531
exeqtion <67519576+exeqtion@users.noreply.github.com>
2023-03-07 17:06:51 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "chicken mutton tandoori";
+        return "hej hej!!!";
     }
 
     public static void main(String[] args) {
