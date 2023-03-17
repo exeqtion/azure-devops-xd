aed26f5bd97f1d6a2e8737a7d7e6980ae472e926
exeqtion <pmunagekar40@gmail.com>
2023-03-17 13:39:45 +0530

    xd

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "HIEEE!!!";
+        return "Ayooo my bois!!!";
     }
 
     public static void main(String[] args) {
