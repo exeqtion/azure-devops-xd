cc24fb3688b82d1f2577b208997770fb9fb0d5f3
exeqtion <pmunagekar40@gmail.com>
2023-03-28 15:52:26 +0530

    xd

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "yo!!!";
+        return "A Beautiful Star!!!";
     }
 
     public static void main(String[] args) {
